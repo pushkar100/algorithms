57be145fe408d3adf945a85be2b9953bac3e4e8a
Pushkar DK <pushkar100@gmail.com>
2020-02-16 20:15:41 +0530

    Create 1-all-distinct-subsets-from-a-distinct-set.js

---
+++ subsets/1-all-distinct-subsets-from-a-distinct-set.js
@@ -0,0 +1,23 @@
+const find_subsets = function(nums) {
+  let subsets = [[]];
+
+  /* BFS */
+  // Loop through the numbers:
+  nums.forEach(num => {
+    let additionalSets = [];
+    // Loop through existing subsets to add 1 more element to each:
+    subsets.forEach(subset => {
+      additionalSets.push([...subset, num]);
+    });
+    // Update subsets with the new sets (pushed to the end):
+    subsets = [...subsets, ...additionalSets];
+    // Reset the additional sets:
+    additionalSets = [];
+  });
+
+  return subsets;
+};
+
+
+console.log(`Here is the list of subsets:`, find_subsets([1, 3]))
+console.log(`Here is the list of subsets:`, find_subsets([1, 5, 3]))
